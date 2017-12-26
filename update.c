d12469ed6052fd95939adfe9c766333c955f0e69
slamdunk <slamdunk1016@outlook.com>
2017-12-26 10:12:34 +0800

    add uboot update Func

--- update.c
+++ update.c
@@ -400,8 +400,8 @@ int update_bt(void){
 		fclose(f_bt1);
 		fclose(f_bt2);
 		umount("/btmp1");
-		umount("/btmp2");
 		remove("/btmp1");
+		umount("/btmp2");
 		remove("/btmp2");
 	}else{
 		fprintf(serial_fp, "[recovery]%s or %s does not exist\n",BT_MAC_BACKUP_PATH, BT_MAC_BACKUP_PATH2);
@@ -434,6 +434,181 @@ int find_update_zip(void)
 		return -1;
 	}
 	return 0;
+}
+#define COMMIT_FLAG_ADDR 0x200000
+char *Uboot_Name = "/sdcard/update/u-boot.csr";
+char *Uboot_Path = "/dev/nandblk0";
+
+char* WriteUbootFn(char* filename,char* partition) {
+    char* result = NULL;
+    long write_position;
+    bool is_emmc = false;
+
+    if (strcmp(partition, "/dev/mmcblk0") == 0) is_emmc = true;
+
+    if (strlen(partition) == 0) {
+        fprintf(stderr, "[WriteUbootFn] partition argument to %s can't be empty", partition);
+        goto done;
+    }
+    if (strlen(filename) == 0) {
+        fprintf(stderr, "[WriteUbootFn] file argument to %s can't be empty", filename);
+        goto done;
+    }
+    bool success;
+    fprintf(stderr, "[WriteUbootFn] open %s\n", filename);
+    FILE* f = NULL;
+    f = fopen(filename, "rb");
+    if (f == NULL) {
+        fprintf(stderr, "[WriteUbootFn]: can't open %s: %s\n",
+                 filename, strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    long read_position;
+    read_position = 0;
+    if (!!(fseek(f, read_position, SEEK_SET))) {
+        fprintf(stderr, "[WriteUbootFn]fseek failed, %s\n", strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    fprintf(stderr, "[WriteUbootFn]fseek to read position: %d\n", (int)read_position);
+    fprintf(stderr, "[WriteUbootFn]open %s\n", partition);
+    FILE* raw_f = NULL;
+    raw_f = fopen(partition,"r");
+    if (raw_f == NULL) {
+        fprintf(stderr, "[WriteUbootFn]: can't open %s: %s\n",
+               partition, strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    fclose(raw_f);
+    raw_f = NULL;
+    raw_f = fopen(partition,"wb");
+    if (raw_f == NULL) {
+        fprintf(stderr, "[WriteUbootFn]: can't open %s: %s\n",
+                partition, strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    int fd;
+    fd = open(partition, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "[WriteUbootFn]: can't open %s: %s\n",
+                partition, strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    ioctl(fd, BLKSSZGET, &write_position);
+    fprintf(stderr, "[WriteUbootFn]store uboot at offset %ld of %s\n",
+        write_position, strerror(errno));
+    close(fd);
+    if (!!(fseek(raw_f, write_position, SEEK_SET))) {
+        fprintf(stderr, "[WriteUbootFn]fseek failed, %s\n", strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    fprintf(stderr, "[WriteUbootFn]fseek to write position: %d\n", (int)write_position);
+    success = true;
+    char* buffer = malloc(BUFSIZ);
+    if (buffer == NULL) {
+        success = false;
+        fprintf(stderr, "failed to alloc %d bytes", BUFSIZ);
+        goto done_malloc;
+    }
+    int read;
+    int wrote;
+    int read_img_count = 0;
+    int write_img_count = 0;
+    while (success && (read = fread(buffer, 1, BUFSIZ, f)) > 0) {
+        read_img_count += read;
+        wrote = fwrite(buffer, 1, read, raw_f);
+        write_img_count += wrote;
+        success = success && (wrote == read);
+        if (!success) {
+            fprintf(stderr, "[WriteUbootFn]write to %s: %s\n",
+                    partition, strerror(errno));
+        }
+    }
+    fflush(raw_f);
+    fsync(fileno(raw_f));
+
+    /* Write the backup uboot from 1MB + 1 sector for EMMC
+     * NANDDISK do the backup internal
+     * */
+    if (is_emmc) {
+	write_position += 1024 * 1024;
+        if (!!(fseek(raw_f, write_position, SEEK_SET))) {
+            fprintf(stderr, "[WriteUbootFn]fseek failed, %s\n", strerror(errno));
+            result = strdup("");
+            goto done;
+        }
+        fprintf(stderr, "[WriteUbootFn]fseek to write position: %d\n", (int)write_position);
+        success = true;
+	memset(buffer, 0, BUFSIZ);
+        read = 0;
+	wrote = 0;
+        read_img_count = 0;
+        write_img_count = 0;
+        while (success && (read = fread(buffer, 1, BUFSIZ, f)) > 0) {
+            read_img_count += read;
+            wrote = fwrite(buffer, 1, read, raw_f);
+            write_img_count += wrote;
+            success = success && (wrote == read);
+            if (!success) {
+                fprintf(stderr, "[WriteUbootFn]write to %s: %s\n",
+                        partition, strerror(errno));
+            }
+        }
+        fflush(raw_f);
+        fsync(fileno(raw_f));
+    }
+
+    // write commit flag
+    if (!!(fseek(raw_f, COMMIT_FLAG_ADDR, SEEK_SET))) {
+        fprintf(stderr, "[WriteUbootFn]fseek failed, %s\n", strerror(errno));
+        result = strdup("");
+        goto done;
+    }
+    success = success && (1 == fwrite(buffer, 1, 1, raw_f));
+    if (!success) {
+        fprintf(stderr, "[WriteUbootFn]write to %s: %s\n",
+                partition, strerror(errno));
+    }
+
+    free(buffer);
+
+    /* Write uboot update flag(0x3c) at the 2.5MB-4096+1 byte.
+     * Uboot will check it to know if uboot has been updated.
+     */
+    int flag_offset = 0x0027f000 + 1;
+    int flag = 0x3c;
+    if (!(fseek(raw_f, flag_offset, SEEK_SET))) {
+	fprintf(stderr, "[WriteUbootFn]set Uboot update flag %#x at %#x\n",flag, flag_offset);
+	putc(flag, raw_f);
+    } else {
+        fprintf(stderr, "[WriteUbootFn]fseek Uboot update flag offset failed, %s\n", strerror(errno));
+    }
+    fflush(raw_f);
+    fsync(fileno(raw_f));
+	fprintf(stderr, "[WriteUbootFn]fsync done! \n");
+done_malloc:
+    fclose(f);
+    f = NULL;
+    fclose(raw_f);
+    raw_f = NULL;
+
+    fprintf(stderr,"[WriteUbootFn]%s %s partition from %s\n",
+           success ? "wrote" : "failed to write", partition, filename);
+
+    result = success ? partition : strdup("");
+
+done:
+    if (result != partition) {
+		free(partition);
+		free(filename);
+		}
+	fprintf(stderr, "[WriteUbootFn]write u-boot.csr successed! \n");
+    return result;
 }
 
 int	main(int argc, char **argv) {
@@ -472,7 +647,7 @@ int	main(int argc, char **argv) {
 
 	/*******************/
 	/*******************/
-
+#if 0
 	else if ((f = fopen(user_part_sd, "rb")) != NULL) {
         /* sd boot and updated from its own user partition */
         target_media = "/dev/mmcblk0";
@@ -497,6 +672,7 @@ int	main(int argc, char **argv) {
         }
 		fprintf(serial_fp, "[recovery]Recovery from Nand internal user partition to %s\n", target_media);
     }
+#endif
 
     mkdir("/sdcard/cache", 755);
     const char *send_intent = NULL;
@@ -513,7 +689,7 @@ int	main(int argc, char **argv) {
     update_package = UPDATE_PACKAGE_PATH;
 	//status = install_package(UPDATE_PACKAGE_PATH, &wipe_cache, TEMPORARY_INSTALL_FILE);
 
-		
+		WriteUbootFn(Uboot_Name,Uboot_Path);
 /* update update.zip */
 	if(status == INSTALL_SUCCESS) {
             sprintf(user_part, "%sp7", target_media);
@@ -525,6 +701,7 @@ int	main(int argc, char **argv) {
 		copy_file(update_package, UPDATEZIP_PATH);
 	        fprintf(serial_fp, "[recovery]copy update.zip done!\n");
 	    }
+		remove("/user");
 	}
 /*restore BT files*/
 	if(status == INSTALL_SUCCESS){
@@ -547,6 +724,7 @@ int	main(int argc, char **argv) {
         fprintf(serial_fp, "[recovery]For more log information, please check %s now\n", TEMPORARY_LOG_FILE);    	 
 		}
 	if(0==umount("/sdcard"))
+		remove("/sdcard");
 		fprintf(serial_fp, "[recovery]umount /sdcard done...\n");
     return EXIT_SUCCESS;
 }
